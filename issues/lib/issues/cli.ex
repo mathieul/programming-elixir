7e5d1dfcb4a95078c235cf2f093a4115d2295412
Mathieu Lajugie <mathieul@gmail.com>
2013-06-26 21:40:30 -0700

    issues: add process function

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -2,9 +2,10 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
-
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean ],
@@ -16,4 +17,16 @@ defmodule Issues.CLI do
       _                               -> :help
     end
   end
+
+  def process(:help) do
+    IO.puts """
+    Usage: issues <user> <project> [ count / #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+
 end
