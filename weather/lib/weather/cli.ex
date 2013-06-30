7362f8a33e826cd1f030425d52656e92ded553f4
Mathieu Lajugie <mathieul@gmail.com>
2013-06-29 23:14:24 -0700

    cleanup async processing a bit

--- weather/lib/weather/cli.ex
+++ weather/lib/weather/cli.ex
@@ -29,25 +29,26 @@ defmodule Weather.CLI do
   end
 
   def process({ location_codes, true }) do
-    me = self
-    printer = spawn_link fn ->
-      column_widths = [4, 35, 30, 20, 16]
-      format = Weather.TableFormatter.format_for(column_widths)
-      Weather.TableFormatter.puts_one_line_in_columns(@headers, format)
-      IO.puts Weather.TableFormatter.separator(column_widths)
-      Enum.each location_codes, fn _code ->
-        receive do
-          { :weather, row } ->
-            Weather.TableFormatter.puts_in_columns([row], @headers, format)
-        end
-      end
-      me <- :done
-    end
+    printer = spawn_link(Weather.CLI, :run_printer, [self, length(location_codes)])
     Enum.each location_codes, fn code ->
       spawn_link fn ->
         (printer <- { :weather, Weather.NOAA.get_current_weather(code) })
       end
     end
-    receive do _ -> IO.puts "Done." end
+    receive do :printer_done -> end
+  end
+
+  def run_printer(processor, num_rows) do
+    column_widths = [4, 35, 30, 20, 16]
+    format = Weather.TableFormatter.format_for(column_widths)
+    Weather.TableFormatter.puts_one_line_in_columns(@headers, format)
+    IO.puts Weather.TableFormatter.separator(column_widths)
+    Enum.each 1..num_rows, fn _code ->
+      receive do
+        { :weather, row } ->
+          Weather.TableFormatter.puts_in_columns([row], @headers, format)
+      end
+    end
+    processor <- :printer_done
   end
 end
