078b665c2fae539c06da359cfce4f2e57fdbed6a
Mathieu Lajugie <mathieul@gmail.com>
2013-07-02 21:53:59 -0700

    add ticker

---
+++ ticker.ex
@@ -0,0 +1,44 @@
+defmodule Tick do
+
+  @interval 2000
+  @name :ticker
+
+  def start do
+    pid = spawn(__MODULE__, :generator, [[]])
+    :global.register_name(@name, pid)
+  end
+
+  def register(client_id) do
+    :global.whereis_name(@name) <- { :register, client_id }
+  end
+
+  def generator(clients) do
+    receive do
+      { :register, pid } ->
+        IO.puts "registering #{inspect pid}"
+        generator([ pid | clients ])
+
+      after @interval ->
+        IO.puts "tick"
+        Enum.each clients, fn client ->
+          client <- { :tick }
+        end
+        generator(clients)
+    end
+  end
+end
+
+defmodule Client do
+  def start do
+    pid = spawn(__MODULE__, :receiver, [])
+    Tick.register(pid)
+  end
+
+  def receiver do
+    receive do
+      { :tick } ->
+        IO.puts "tock in client #{inspect self}"
+        receiver
+    end
+  end
+end
